42f0f4f5cc215421db9630807c3a58c87994dfda
Bemal <bimal163@gmail.com>
2024-03-01 16:15:15 +0400

    Компонент Категорий и фильтрация по ним

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,32 +1,54 @@
 import { useEffect, useState } from "react";
 import NewsBanner from "../components/NewsBanner/NewsBanner";
 import styles from "./styles.module.css";
-import { getNews } from "../api/apiNews";
+import { getCategories, getNews } from "../api/apiNews";
 import NewsList from "../components/NewsList/NewsList";
 import Skeleton from "../components/Skeleton/Skeleton";
 import { Pagination } from "../components/Pagination/Pagination";
+import { Categories } from "../components/Categories/Categories";
 
 const Main = () => {
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
+  const [categories, setCategories] = useState([]);
+  const [selectCategories, setSelectCategories] = useState("All");
   const totalPage = 10;
   const pageSize = 10;
 
   const fetchNews = async (currentPage) => {
     try {
       setIsLoading(true);
-      const response = await getNews(currentPage, pageSize);
-      setNews(response.news);
+      const responseNews = await getNews({
+        page_number: currentPage,
+        page_size: pageSize,
+        category: selectCategories === "All" ? null : selectCategories,
+      });
+      setNews(responseNews.news);
       setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
   };
 
+  const fetchCategories = async () => {
+    try {
+      const responseCategories = await getCategories();
+      setCategories(["All", ...responseCategories.categories]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(categories);
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
   useEffect(() => {
     fetchNews(currentPage);
-  }, [currentPage]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPage, selectCategories]);
 
   const hendleNextPage = () => {
     if (currentPage < totalPage) {
@@ -46,6 +68,11 @@ const Main = () => {
 
   return (
     <main className={styles.main}>
+      <Categories
+        categories={categories}
+        setSelectCategories={setSelectCategories}
+        selectCategories={selectCategories}
+      />
       {news.length > 0 && !isLoading ? (
         <NewsBanner item={news[0]} />
       ) : (
