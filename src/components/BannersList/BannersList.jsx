60b1834b8c15f64aceca4e0bba9c298185f8282a
Bemal <bimal163@gmail.com>
2024-03-06 14:36:29 +0400

    Рефактор кода, декомпозиция компонентов

---
+++ src/components/BannersList/BannersList.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import withSkeleton from "../../helpers/hocs/withSkeleton";
+import NewsBanner from "../NewsBanner/NewsBanner";
+import styles from "./styles.module.css";
+
+const BannersList = ({ banners }) => {
+  return (
+    <ul className={styles.banners}>
+      {banners.map((banner) => {
+        return <NewsBanner key={banner.id} item={banner} />;
+      })}
+    </ul>
+  );
+};
+
+const BannersListWithSkeleton = withSkeleton(BannersList, "banner", 10, "row");
+
+export default BannersListWithSkeleton;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,108 +1,38 @@
-import { useEffect, useState } from "react";
-import NewsBanner from "../components/NewsBanner/NewsBanner";
+
 import styles from "./styles.module.css";
-import { getCategories, getNews } from "../api/apiNews";
-import NewsList from "../components/NewsList/NewsList";
-import Skeleton from "../components/Skeleton/Skeleton";
-import { Pagination } from "../components/Pagination/Pagination";
-import { Categories } from "../components/Categories/Categories";
-import { Search } from "../components/Search/Search";
+import { getNews } from "../api/apiNews";
 import { useDebounce } from "../helpers/hooks/useDebounced";
+import { useFetch } from "../helpers/hooks/useFetch";
+import { PAGE_SIZE } from "../constants/constants";
+import { useFilters } from "../helpers/hooks/useFilters";
+import { LatestNews } from "../components/LatestNews/LatestNews";
+import NewsByFilter from "../components/NewsByFilter/NewsByFilter";
 
 const Main = () => {
-  const [news, setNews] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [categories, setCategories] = useState([]);
-  const [selectCategories, setSelectCategories] = useState("All");
-  const [keywords, setKeywords] = useState("")
-  const totalPage = 10;
-  const pageSize = 10;
-  const debouncedKeywords = useDebounce(keywords, 1500)
+  const { filters, changeFilters } = useFilters({
+    page_number: 1,
+    page_size: PAGE_SIZE,
+    category: null,
+    keywords: "",
+  });
 
-  const fetchNews = async (currentPage) => {
-    try {
-      setIsLoading(true);
-      const responseNews = await getNews({
-        page_number: currentPage,
-        page_size: pageSize,
-        category: selectCategories === "All" ? null : selectCategories,
-        keywords: debouncedKeywords,
-      });
-      setNews(responseNews.news);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const debouncedKeywords = useDebounce(filters.keywords, 1500);
 
-  const fetchCategories = async () => {
-    try {
-      const responseCategories = await getCategories();
-      setCategories(["All", ...responseCategories.categories]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const { data, isLoading } = useFetch(getNews, {
+    ...filters,
+    keywords: debouncedKeywords,
+  });
 
-  useEffect(() => {
-    fetchCategories();
-  }, []);
-
-  useEffect(() => {
-    fetchNews(currentPage);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentPage, selectCategories, debouncedKeywords]);
-
-  const hendleNextPage = () => {
-    if (currentPage < totalPage) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const hendlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-
-  const hendlePageClick = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
-  console.log(keywords)
+  console.log(filters.keywords);
 
   return (
     <main className={styles.main}>
-      <Categories
-        categories={categories}
-        setSelectCategories={setSelectCategories}
-        selectCategories={selectCategories}
-      />
-      <Search keywords={keywords} setKeywords={setKeywords} />
-      {news.length > 0 && !isLoading ? (
-        <NewsBanner item={news[0]} />
-      ) : (
-        <Skeleton type={"banner"} count={1} />
-      )}
-      <Pagination
-        hendlePrevPage={hendlePrevPage}
-        hendleNextPage={hendleNextPage}
-        hendlePageClick={hendlePageClick}
-        totalPage={totalPage}
-        currentPage={currentPage}
-      />
-      {!isLoading ? (
-        <NewsList news={news} />
-      ) : (
-        <Skeleton type={"item"} count={10} />
-      )}
-      <Pagination
-        hendlePrevPage={hendlePrevPage}
-        hendleNextPage={hendleNextPage}
-        hendlePageClick={hendlePageClick}
-        totalPage={totalPage}
-        currentPage={currentPage}
+      <LatestNews isLoading={isLoading} banners={data && data.news} />
+      <NewsByFilter
+        news={data?.news}
+        isLoading={isLoading}
+        filters={filters}
+        changeFilters={changeFilters}
       />
     </main>
   );
